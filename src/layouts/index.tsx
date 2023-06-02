a197ec5e4e068bb980646a65c0e8f04049ee6151
kasodesyn <bokang.qian@desyn.io>
2023-06-02 13:21:51 +0800

    i

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,9 +1,11 @@
 import { Link, Outlet, useModel } from "umi";
 import styles from "./index.less";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Layout() {
   const { logs, clearLogs, downloadLogs } = useModel("logModel");
+
+  const [isLogOpen, setIsLogOpen] = useState<boolean>(true);
 
   useEffect(() => {
     updateScrollPosition();
@@ -15,6 +17,7 @@ export default function Layout() {
       logsDOM.scrollTop = logsDOM.scrollHeight;
     }
   };
+
   return (
     <div className=" p-3 px-10 bg-gray-100 border ">
       <div className=" flex gap-5 border font-bold underline">
@@ -24,25 +27,45 @@ export default function Layout() {
         <Link to="/interacted">合约交互</Link>
       </div>
       <div className="  mt-3 p-4 relative">
-        <div className=" absolute bg-[rgba(0,0,0,.8)] w-64 rounded font-mono text-white p-2 text-xs right-0 top-0">
-          <div className=" flex gap-2 font-bold leading-6 text-[rgba(255,255,255,1)]">
-            <div className=" underline cursor-pointer" onClick={clearLogs}>
-              清除日志
+        {isLogOpen ? (
+          <div className=" absolute bg-[rgba(0,0,0,.8)] w-96 rounded font-mono text-white p-2 text-xs right-0 top-0">
+            <div className=" flex justify-between">
+              <div className=" flex gap-2 font-bold leading-6 text-[rgba(255,255,255,1)]">
+                <div className=" underline cursor-pointer" onClick={clearLogs}>
+                  清除日志
+                </div>
+                <div
+                  className=" underline cursor-pointer"
+                  onClick={downloadLogs}
+                >
+                  下载日志
+                </div>
+              </div>
+              <div
+                className=" underline cursor-pointer"
+                onClick={() => setIsLogOpen(false)}
+              >
+                隐藏
+              </div>
             </div>
-            <div className=" underline cursor-pointer" onClick={downloadLogs}>
-              下载日志
+            <hr />
+            <div
+              id="logs"
+              className="h-48 py-1 overflow-y-scroll text-[rgba(255,255,255,1)]"
+            >
+              {logs.map((item) => {
+                return <div>{item}</div>;
+              })}
             </div>
           </div>
-          <hr />
+        ) : (
           <div
-            id="logs"
-            className="h-48 py-1 overflow-y-scroll text-[rgba(255,255,255,1)]"
+            className=" underline absolute bg-[rgba(0,0,0,.8)] cursor-pointer rounded font-mono text-white p-2 text-xs right-0 top-0"
+            onClick={() => setIsLogOpen(true)}
           >
-            {logs.map((item) => {
-              return <div>{item}</div>;
-            })}
+            显示日志
           </div>
-        </div>
+        )}
         <Outlet />
       </div>
     </div>
